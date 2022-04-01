5a78f818b0f919905d3202cb06c46fa575dd413e
Kapil Bisen <kapil@metronlabs.io>
2022-04-01 18:12:08 +0530

    Use a single file share with 2 different files

--- Solutions/Digital Shadows/Data Connectors/Digital Shadows/DigitalShadowsConnectorAzureFunction/__init__.py
+++ Solutions/Digital Shadows/Data Connectors/Digital Shadows/DigitalShadowsConnectorAzureFunction/__init__.py
@@ -17,6 +17,7 @@ historical_days = os.environ['HistoricalDays']
 url = os.environ['DigitalShadowsURL']
 classification_filter_operation = os.environ['ClassificationFilterOperation']
 high_variability_classifications = os.environ['HighVariabilityClassifications']
+function_name = os.environ['FUNCTION_NAME']
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -28,7 +29,7 @@ def main(mytimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
     
-    DSobj = DS_poller.poller(account_id, key, secret, customer_id, shared_key, connection_string, historical_days, url)
+    DSobj = DS_poller.poller(function_name, account_id, key, secret, customer_id, shared_key, connection_string, historical_days, url)
     classification_list = high_variability_classifications.split(",")
 
     DSobj.poll(classification_filter_operation, classification_list)
